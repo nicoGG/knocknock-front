ea1e92347744ce46fcd41c4c342c24f567f88cfd
Nicolas Galdames <nigalval@gmail.com>
2026-08-04 17:22:18 -0400

    :hammer: Change url default

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -1,11 +1,11 @@
 abstract final class AppConfig {
   static const apiBaseUrl = String.fromEnvironment(
     'API_BASE_URL',
-    defaultValue: 'http://localhost:4000/api',
+    defaultValue: 'https://knocknock-backend-production.up.railway.app/api',
   );
 
   static const socketBaseUrl = String.fromEnvironment(
     'SOCKET_BASE_URL',
-    defaultValue: 'http://localhost:4000',
+    defaultValue: 'https://knocknock-backend-production.up.railway.app',
   );
 }
